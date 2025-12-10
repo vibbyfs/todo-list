81750228f816def20d6a792822a277783751d00a
vibbyfs <vibbyfs@gmail.com>
2025-12-10 19:19:30 +0700

    feat: configure TypeORM with PostgreSQL connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'TODOS_DB',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
