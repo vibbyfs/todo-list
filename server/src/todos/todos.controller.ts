4e6017446fff6b67cc0ae58ebfd2976152c97ebb
vibbyfs <vibbyfs@gmail.com>
2025-12-10 20:32:40 +0700

    feat: implement TodosController to handle HTTP requests via service

--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { Todo } from './entities/todos.entity';
+import { CreateTodoDto } from './dto/create.todo.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todoService: TodosService) {}
+
+  @Get()
+  findAll(@Query('search') search?: string): Promise<Todo[]> {
+    return this.todoService.findAll(search);
+  }
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoService.create(createTodoDto);
+  }
+}
