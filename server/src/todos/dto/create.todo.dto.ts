fc125681ade51e7f10ed5274571e47ce4a1155aa
vibbyfs <vibbyfs@gmail.com>
2025-12-10 19:29:08 +0700

    feat: connect TodosModule with TypeORM repository

---
+++ server/src/todos/dto/create.todo.dto.ts
@@ -0,0 +1,3 @@
+export class createTodoDto {
+  title: string;
+}
--- server/src/todos/todos.module.ts
+++ server/src/todos/todos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './entities/todos.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodosController],
-  providers: [TodosService]
+  providers: [TodosService],
 })
 export class TodosModule {}
