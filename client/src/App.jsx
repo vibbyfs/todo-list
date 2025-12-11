9ed22a6a146707510fab6a74a8459e6e41a53909
vibbyfs <vibbyfs@gmail.com>
2025-12-11 15:26:44 +0700

    setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
